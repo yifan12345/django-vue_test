83e07a8ee5d1659ac14dcd00c89d402b81dc2eaa
yifan <yifan1563@163.com>
2020-06-03 12:54:07 +0800

    新建项目

---
+++ vue_i_project/src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <HelloWorld msg="hello">
+
+        </HelloWorld>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from "../components/HelloWorld";
+    export default {
+        name: "home",
+        props:{
+            arg :String,
+        },
+        components:{
+            // eslint-disable-next-line vue/no-unused-components
+            HelloWorld
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
